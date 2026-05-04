5957a2eb3fd9f161342c769384476fe57f040b8c
unknown <raynara.oliveira10@aluno.ce.gov.br>
2026-05-04 14:52:14 -0300

    Mudei o top bar o dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                     </svg>
-                    <span class="text-[10px] font-bold uppercase tracking-[0.15em] whitespace-nowrap">Dashboard</span>
+                    <span class="text-[10px] font-bold uppercase tracking-[0.15em] whitespace-nowrap">Pagina inicial</span>
                 </a>
 
                 <a href="/workouts"
